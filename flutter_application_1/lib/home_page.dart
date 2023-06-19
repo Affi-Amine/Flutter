dbdf175837c64442ffa6dc52114e92014054c532
Maynewi <affi.amin.work@gmail.com>
2023-06-19 16:14:55 +0100

    description page stater

--- flutter_application_1/lib/home_page.dart
+++ flutter_application_1/lib/home_page.dart
@@ -5,6 +5,67 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Wrap(
+            spacing: 10,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  shape: const StadiumBorder(),
+                ),
+                child: Text("Gym"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  shape: const StadiumBorder(),
+                ),
+                child: Text("Work"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  shape: const StadiumBorder(),
+                ),
+                child: Text("INSAT"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  shape: const StadiumBorder(),
+                ),
+                child: Text("Travelling"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  shape: const StadiumBorder(),
+                ),
+                child: Text("Self-gro"),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            children: [
+              Image.asset('images/logo.png'),
+            ],
+          ),
+        )
+      ]),
+    );
   }
 }
